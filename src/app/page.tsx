cc9b012b35a82df1c43a57b0e5b1381706d371c8
melch-raiol <melchraiol@hotmail.com>
2024-05-21 12:10:37 -0300

    home page upgraded

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import { GameProps } from '@/utils/types/games';
 import Link from 'next/link';
 import Image from "next/image";
 import { BsArrowRightSquare } from 'react-icons/bs'
+import { Input } from '@/components/input';
+import { GameCards } from '@/components/GameCard';
 
 async function getDalyGame() {
   try {
@@ -13,9 +15,18 @@ async function getDalyGame() {
   }
 }
 
+async function getGamesData() {
+  try {
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=games`, { next: {revalidate: 320}})
+    return res.json();
+  } catch (err) {
+    throw new Error("Failed to fetch data")
+  }
+}
+
 export default async function Home() {
   const dalyGame: GameProps = await getDalyGame();
-  
+  const data: GameProps[] = await getGamesData();
 
   return (
     <main className="w-full">
@@ -42,6 +53,18 @@ export default async function Home() {
            </div>
           </section>
         </Link>
+
+          <Input/>
+
+          <h2 className="text-lg font-bold mt-8 mb-5">
+            Jogos para conhecer
+          </h2>
+          <section className="grid gap-7 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+           {data.map( (item) =>(
+            <GameCards key={item.id} data={item}/>
+           ))}
+          </section>
+
       </Container>
     </main>
   );
